830bbcf2702136597f78e59382875f40861a82cb
Theres Scherrer <theresscherrer@gmail.com>
2014-03-12 20:24:39 -0700

    First draft of my portfolio

---
+++ SinglePage/index.php
@@ -0,0 +1,142 @@
+<?php require('header.php'); ?>
+
+<div class="my_container" id="my_container">
+  <div id="page1">
+    <a id="about"></a>
+      <div class="my_container">
+        <h3><span class="courier">&lt;TheCoder&gt;</span> Hello, I am <br><span id="theCoder">The Coder</span><br> and I love what I do <span class="courier">&lt;/TheCoder&gt;</span></h3>
+
+        <h2>If you're looking for a junior web developer, you're in the right place.</h2>
+
+      </div>
+  </div>
+
+  <div id="page2">
+    <a id="current_projects"></a>
+      <div class="my_container">
+        <h2><strong>Latest Work/</strong></h2>
+        <div class="row">
+          <div class="col-md-4" >
+            <div class="text_height">
+              <h3 class="title">Rainbow Mural</h3>
+              <p>Explore street art around the world.<br>Like your favorite photos and engage in conversations with other users.</p>
+            </div>
+            <div class="project">
+              <a href="http://www.rainbowmural.com" target="_blank"><img src="images/rainbow_slider2.png"></a>
+              <p><strong>technologies</strong> PHP, MySQL, jQuery, Bootstrap, Flickr API, Google Maps API, and Heroku.</p>
+              <a href="https://github.com/therezajs/rainbowmural" target="_blank"><span class="glyphicon glyphicon-hand-right"></span> source code</a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="text_height">
+              <h3 class="title">Kiseki</h3>
+              <p>Facebook clone.<br>Post, find friends, send messages to other user and upload pictueres.</p>
+            </div>
+            <div class="project">
+              <a href="http://kiseki.herokuapp.com" target="_blank"><img src="images/kiseki_profile.png"></a>
+              <p><strong>technologies</strong>Rails, AngularJS, Postgres, Bootstrap, Amazon S3, and Heroku</p>
+              <a href="https://github.com/therezajs/CodingDojo/tree/master/projects/Mixi" target="_blank"><span class="glyphicon glyphicon-hand-right"></span> source code</a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="text_height">
+              <h3 class="title">Code the Games</h3>
+              <p>Play TicTacToe ot the sanke game.<br>Code is tested with rspec</p>
+            </div>
+            <div class="project">
+              <a href="http://codezegame.herokuapp.com" target="_blank"><img src="images/zegame_index.png"></a>
+              <p><strong>technologies</strong>Rails, Javascript, Rspec, Bootstrap, and Heroku.</p>
+              <a href="https://github.com/therezajs/CodeTheGame" target="_blank"><span class="glyphicon glyphicon-hand-right"></span> source code</a>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+
+  <div id="page3">
+    <a id="mini_projects"></a>
+      <div class="my_container">
+        <h2>Mini Projects/</h2>
+        <div class="row">
+          <div class="col-md-4">
+            <h3 class="title">filtering contact info</h3>
+            <div class="project">
+              <a href="http://www.rainbowmural.com" target="_blank"><img src="images/filtering_contacts_data.png"></a>
+              <p><strong>technologies</strong> html, css, php, mysql, ajax, jquery, jquery UI</p>
+              <a href="https://github.com/therezajs/CodingDojo/tree/master/exercises/6_ajax_exercises/demo" target="_blank"><span class="glyphicon glyphicon-hand-right"></span> source code</a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <h3 class="title">weather report</h3>
+            <div class="project">
+              <a href="http://www.rainbowmural.com" target="_blank"><img src="images/weather_report.png"></a>
+              <p><strong>technologies</strong> html, css, world weather API, ajax</p>
+              <a href="https://github.com/therezajs/CodingDojo/tree/master/exercises/6_ajax_exercises/weather_directions" target="_blank"><span class="glyphicon glyphicon-hand-right"></span> source code</a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <h3 class="title">the "facebook" wall</h3>
+            <div class="project">
+              <a href="http://www.rainbowmural.com" target="_blank"><img src="images/the_wall.png"></a>
+              <p><strong>technologies</strong> html, css, php, mysql</p>
+              <a href="https://github.com/therezajs/CodingDojo/tree/master/exercises/5_php_exercises/wall" target="_blank"><span class="glyphicon glyphicon-hand-right"></span> source code</a>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <h3 class="title">friends finder</h3>
+            <div class="project">
+              <a href="http://www.rainbowmural.com" target="_blank"><img src="images/friends_finder.png"></a>
+              <p><strong>technologies</strong> html, css, php, mysql, oop</p>
+              <a href="https://github.com/therezajs/CodingDojo/tree/master/exercises/7_OOP/advanced" target="_blank"><span class="glyphicon glyphicon-hand-right"></span> source code</a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <h3 class="title">mini game</h3>
+            <div class="project">
+              <a href="http://www.rainbowmural.com" target="_blank"><img src="images/mini_game.png"></a>
+              <p><strong>technologies</strong> html, css, php, mysql</p>
+              <a href="https://github.com/therezajs/CodingDojo/tree/master/exercises/5_php_exercises/mini_game" target="_blank"><span class="glyphicon glyphicon-hand-right"></span> source code</a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <h3 class="title">post its</h3>
+            <div class="project">
+              <a href="http://www.rainbowmural.com" target="_blank"><img src="images/post-its.png"></a>
+              <p><strong>technologies</strong> html, css, php, mysql, ajax</p>
+              <a href="https://github.com/therezajs/CodingDojo/tree/master/exercises/6_ajax_exercises/Basic_1" target="_blank"><span class="glyphicon glyphicon-hand-right"></span> source code</a>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+
+  <div id="page4">
+    <a id="contact"></a>
+      <div class="my_container">
+      <h2>About me/</h2>
+        <div class="row">
+          <div class="col-md-4">
+            <div style="text-align:right;">
+              <img src="images/avatar.png" class="img-circle my_avatar">
+            </div>
+          </div>
+          <div class="col-md-6">
+            <p>Hello, I am Thereza and I am looking for a job as web developer.
+            I started to teach myself programming on Codecademy and fell in love with it.
+            I moved to San Francisco in October 2013 to attend a programming bootcamp, <a href="http://codingdojo.com/">Coding Dojo</a>.</p>
+
+            <p>I built this project with PHP, jQuery and MySQL.
+              For the pictures, I used the flickr API and I worked with the Google API to display all the maps.</p>
+            <br>
+            <p>To learn more about me, check out my <button class="btn btn-success"><a href="http://www.linkedin.com/in/thereza">LinkedIn</a></button>, <button class="btn btn-success"><a href="https://twitter.com/therezaJS">Twitter</a></button> and <button class="btn btn-success"><a href="https://github.com/therezajs">Github</a></button> account.</p>
+        </div>
+        <!-- <div class="col-md-3">
+
+        </div> -->
+      </div>
+  </div>
+</div>
+
+<?php require('footer.php'); ?>
